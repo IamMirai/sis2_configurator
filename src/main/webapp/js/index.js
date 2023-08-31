e2ac2167f582538547977a3624c7dc1ca8a05a69
Jbar <julenbara6@gmail.com>
2023-08-31 11:14:17 +0200

    Basic CSS added and workspace field added to HTML login view

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,9 +1,17 @@
-function HidePassword(){
-	var input_password = document.getElementById('input_password');
-	
-	if(input_password.type === 'password'){
+var btn_hide_show_password = document.querySelector('.btn_hide_show_password');
+
+btn_hide_show_password.addEventListener('click', function() {
+
+	var input_password = document.querySelector('.input_password');
+
+	if (input_password.type === 'password') {
 		input_password.type = 'text';
-	}else if(input_password.type === 'text'){
+		btn_hide_show_password.textContent = 'Show password';
+
+	} else if (input_password.type === 'text') {
 		input_password.type = 'password';
+		btn_hide_show_password.textContent = 'Hide password';
 	}
-}
+
+});
+
